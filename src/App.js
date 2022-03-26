c2208754332104641f71cdc580c97b62f23e9f08
William Bowdler <callowaybowdler@gmail.com>
2022-03-26 14:56:40 -0500

    creating a dynamic list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 function App() {
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    {id: 1, text: 'Comment one'},
+    {id: 2, text: 'Comment two'},
+    {id: 3, text: 'Comment three'},
+  ]
+
   return (
-    <h1>For God so loved the world that He gave His only Son</h1>
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment) => {
+            return <li>{comment.text}</li>
+          })}
+        </ul>
+      </div>
+    </div>
+
+    
   )
 }
 
