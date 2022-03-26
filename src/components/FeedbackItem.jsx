cefb2e5f5e43368a9a35994be79fe5fad665b3f0
William Bowdler <callowaybowdler@gmail.com>
2022-03-26 16:25:17 -0500

    added a conditional class for reverse styling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import Card from './shared/Card'
 
 function FeedbackItem({ item }) {
   return (
-    <div className="card">
+    <Card reverse={true}>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   )
 }
 
