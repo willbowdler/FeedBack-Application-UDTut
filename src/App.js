a357420ada6d2e6ba5ce9b897cb29bf7d9d437e2
William Bowdler <callowaybowdler@gmail.com>
2022-04-09 13:43:47 -0500

    using context to clean up code 2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import { v4 as uuidv4 } from 'uuid'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
 import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './pages/AboutPage'
-import { FeebackProvider } from './context/FeedbackContext'
-
-import FeedbackData from "./data/FeedbackData"
+import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-  }
-
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
 
   return (
-    <FeebackProvider>
+    <FeedbackProvider>
       <Router>
         <Header />
         <div className="container">
@@ -36,9 +20,9 @@ function App() {
               path='/'
               element= {
                 <>
-                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackForm />
                   <FeedbackStats />
-                  <FeedbackList handleDelete={deleteFeedback}/>
+                  <FeedbackList />
                 </>
               }>
             </Route>
@@ -49,7 +33,7 @@ function App() {
           <AboutIconLink />
         </div>
       </Router>
-    </FeebackProvider>
+    </FeedbackProvider>
   )
 }
 
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react"
-import RatingSelect from "./RatingSelect";
+import { useState, useContext } from "react"
+import RatingSelect from "./RatingSelect"
 import Card from "./shared/Card"
-import Button from "./shared/Button";
+import Button from "./shared/Button"
+import FeedbackContext from "../context/FeedbackContext"
 
-function FeedbackForm({handleAdd}) {
+function FeedbackForm() {
   const [text, setText] = useState('');
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
+
+  const {addFeedback} = useContext(FeedbackContext)
 
   const handleTextChange = (e) => {
     if (text === '') {
@@ -32,7 +35,8 @@ function FeedbackForm({handleAdd}) {
         rating
       }
 
-      handleAdd(newFeedback)
+      addFeedback(newFeedback)
+      
       setText('')
     }
   }
