daaf8e21c7a37fe7e4466773288d37375a8a42b5
William Bowdler <callowaybowdler@gmail.com>
2022-04-09 12:12:21 -0500

    Using the link component from React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { v4 as uuidv4 } from 'uuid'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './pages/AboutPage'
 
 import FeedbackData from "./data/FeedbackData"
@@ -27,13 +28,23 @@ function App() {
     <Router>
       <Header />
       <div className="container">
-        <Route exact path='/'>
-          <FeedbackForm handleAdd={addFeedback} />
-          <FeedbackStats feedback={feedback}/>
-          <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-        </Route>
+        <Routes>
+          <Route 
+            exact 
+            path='/'
+            element= {
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+              </>
+            }>
+          </Route>
 
-        <Route exact path='/about' component={AboutPage} />
+          <Route exact path='/about' element={<AboutPage />} />
+        </Routes>
+
+        <AboutIconLink />
       </div>
     </Router>
   )
