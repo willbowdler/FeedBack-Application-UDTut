43802be2db3c3a3ee580f59e56113897a8b5be8a
William Bowdler <callowaybowdler@gmail.com>
2022-04-09 11:45:05 -0500

    Adding React Router and route page about me

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from './pages/AboutPage'
 
 import FeedbackData from "./data/FeedbackData"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
@@ -16,14 +24,18 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm />
-        <FeedbackStats feedback={feedback}/>
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+        <Route exact path='/'>
+          <FeedbackForm handleAdd={addFeedback} />
+          <FeedbackStats feedback={feedback}/>
+          <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+        </Route>
+
+        <Route exact path='/about' component={AboutPage} />
       </div>
-    </>
+    </Router>
   )
 }
 
