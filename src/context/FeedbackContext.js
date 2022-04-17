394ac5af9192b14076dfb0443109711b9c5512a8
William Bowdler <callowaybowdler@gmail.com>
2022-04-16 19:38:53 -0500

    This is my last commit. This project is complete. Connected the app to JSON server. Praise the Lord

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -17,7 +17,7 @@ useEffect(() => {
 
 // Fetch feedback
 const fetchFeedback = async () => {
-  const response = await fetch('http://localhost:5000/feedback?_sort=id&_ordere=desc')
+  const response = await fetch('/feedback?_sort=id&_ordere=desc')
   const data = await response.json()
 
   setFeedback(data)
@@ -25,22 +25,42 @@ const fetchFeedback = async () => {
 }
 
 // Add Feedback
-const addFeedback = (newFeedback) => {
+const addFeedback = async (newFeedback) => {
+  const response = await fetch(`/feedback`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newFeedback),
+  })
+
   newFeedback.id = uuidv4()
   setFeedback([newFeedback, ...feedback])
 }
 
 // Delete Feeback
-const deleteFeedback = (id) => {
+const deleteFeedback = async (id) => {
   if (window.confirm('Are you sure you want to delete?')) {
+    await fetch(`/feedback/${id}`, {method: 'DELETE'})
+
     setFeedback(feedback.filter((item) => item.id !== id))
   }
 }
 
 // Update feedback item
-const updateFeedback = (id, updItem) => {
+const updateFeedback = async (id, updItem) => {
+  const response = await fetch(`/feedback/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updItem),
+  })
+
+  const data = await response.json()
+
   setFeedback(
-    feedback.map((item) => item.id === id ? {...item, ...updItem} : item)
+    feedback.map((item) => item.id === id ? {...item, ...data} : item)
   )
 }
 
